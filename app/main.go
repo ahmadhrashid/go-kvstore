30cb1a65bb77750b60dbda6a9a6f4c356122b456
Ahmad Rashid <ahmadhrashid05@gmail.com>
2025-07-14 19:32:16 -0400

    implemented simple pong response

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
@@ -19,9 +20,25 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	// go handleConnection(conn)
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading from connection: ", err.Error())
+		return
+	}
+	if strings.Contains(string(buf[:n]), "PING\r\n"){
+		conn.Write([]byte("+PONG\r\n"))
+	} 
 }
+
+// func handleConnection(conn net.Conn) {
+	
+	
+// }

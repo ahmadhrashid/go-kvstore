4471930d9106a5a389f34c2e972028a85719c887
Ahmad Rashid <ahmadhrashid05@gmail.com>
2025-07-28 20:34:13 -0400

    deleted extra line causing bug

--- app/lists.go
+++ app/lists.go
@@ -14,6 +14,5 @@ func handleRPush(conn io.Writer, commands []string) {
 	for _, val := range commands[2:] {
 		lists[key] = append(lists[key], val)
 	}
-	lists[key] = append(lists[key], commands[2])
 	fmt.Fprintf(conn, ":%d\r\n", len(lists[key]))
 }
